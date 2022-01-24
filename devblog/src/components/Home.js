f50fc90b3f4760daf20ab910c6855f441390eb54
Gonzalo Nicolás <gonnm8@gmail.com>
2022-01-24 18:05:04 +0100

    Home page structure
    Defined the structure of Home. It's using a lot of components that are not defined yet.
    
    Second level components require of implementation.

---
+++ devblog/src/components/Home.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+// Config
+
+// Components
+
+/* HomeHeader corresponds to a card that spans about 1/3 of height on desktop 
+ * and the full page on mobile displays. Shows a picture of the author, a little bit of information
+ * and has a textbox to add in your email to subscribe to the newsletter to know when a new post is up
+ */
+import HomeHeader from './HomeHeader';
+
+/* BlogCard corresponds to a card that contains the title and a few lines of the corresponding blog entry 
+ * aswell as a thumbnail, to give a brief summary of the contents of the blog entry.
+ */
+//import BlogCard from './BlogCard'; I dont need this here. Rather have a component that wraps it
+
+import CardList from './CardList';
+
+/* Miscelanous components */
+import SearchBar from './SearchBar';
+import Spinner from './Spinner';
+
+// Hooks
+
+// Images
+import NoImage from '../images/ef3-placeholder-image.jpg';
+
+// Main Component
+const Home = () => {
+
+    return (
+        <>
+            <HomeHeader />
+            
+            <SearchBar />
+
+            <CardList />
+
+        </>
+    );
+}
+
+export default Home;
+
