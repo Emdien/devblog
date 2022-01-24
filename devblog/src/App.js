78bb83ea4608b24fee1fd60a647087a6ee4301d2
Gonzalo Nicolás <gonnm8@gmail.com>
2022-01-24 16:45:46 +0100

    Main structure of the devblog
    Modified App.js so that it has its main structure, which goes as follows:
    - A header, which will be present in any page of the site
    - A home page, which will be on the index and will have a collection of other different components to be made
    - A page for each blog entry, where it will have the blog entry as the content of the page
    - A 404 page

--- devblog/src/App.js
+++ devblog/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// Routing
+import { BrowserRouter as Router, Routes, Router} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Components: here are the components that App.js makes use of
+import Header from './components/Header';
+import Home from './components/Home';
+import BlogEntry from './components/BlogEntry';
+import NotFound from './components/NotFound';
+
+// Styles: here are the styles that App.js makes use of. Global styles.
+
+const App = () => (
+  <Router>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/:blogEntry' element={<BlogEntry />} />
+      <Route path='/*' element={<NotFound />} />
+    </Routes>
+  </Router>
+)
 
 export default App;
