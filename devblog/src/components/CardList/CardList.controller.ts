16979a6662a7f68a1e554bbf19925281c84764b8
Gonzalo Nicolás <gonnm8@gmail.com>
2022-07-23 12:11:51 +0200

    feat: implemented redux state within the component
    - Cleaned up some of the stuff, such as moving useEffect to the controller, and moving getMarkdownContents inside the useEffect to prevent the need for dependency wraps

--- devblog/src/components/CardList/CardList.controller.ts
+++ devblog/src/components/CardList/CardList.controller.ts
@@ -1,45 +1,48 @@
 
 import matter from "gray-matter";
-import { useState } from "react";
-import { BlogData } from "./BlogCard/BlogCard";
+import { useEffect, useRef} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getBlogEntriesActions } from "../../store/blogEntries";
+import { BlogEntry } from "../../store/blogEntries/getBlogEntries.slice";
+import { RootState } from "../../store/rootReducer";
 window.Buffer = window.Buffer || require("buffer").Buffer;
 
 
 const CardListController = () => {
+    const dispatch = useDispatch();
 
-    const files = ['mockEntry.md', 'mockEntry2.md', 'mockEntry.md', 'mockEntry2.md', ];
 
-    const [ entries, setEntries] = useState<BlogData[]>([])
+    const entrySet = useSelector(
+        (state: RootState) => state.blogEntries.blogEntries.entries
+    )
+
+    const fetchStatus = useSelector(
+        (state: RootState) => state.blogEntries.blogEntries.fetched
+    )
+
+    const loadEntries = useRef(true);
+    useEffect(() => {
+        const files = ['mockEntry.md', 'mockEntry2.md', 'mockEntry.md', 'mockEntry2.md', ];
+        if(loadEntries.current && !fetchStatus){
+            Promise.all(files.map((file) => {
+                return import(`../../entries/${file}`)
+                .then(importedFile => {
+                    return fetch(importedFile.default)
+                })
+                .then(res => res.text())
+                .then(res => matter(res).data)
+                .then(res => dispatch(getBlogEntriesActions.addEntry(res as BlogEntry)))
+                .catch(err => console.log(err))
+            }))
+            loadEntries.current = false
+            dispatch(getBlogEntriesActions.setFetched())
+
+        }
+    }, [dispatch, fetchStatus])
 
     
 
-    const getMarkdownContents = () => {
-        
-        console.log('loading')
-
-        setEntries([])
-
-        Promise.all(files.map((file) => {
-            return import(`../../entries/${file}`)
-            .then(importedFile => {
-                return fetch(importedFile.default)
-            })
-            .then(res => res.text())
-            .then(res => matter(res).data)
-            .then(res => setEntries((prev) => [...prev, res as BlogData]))
-            .catch(err => console.log(err))
-        }))
-
-    }
-
-    const getMatter = (content: string) => {
-        
-        
-
-    }
-    
-
-    return { getMarkdownContents, getMatter, entries, setEntries }
+    return { entrySet }
 }
 
 export default CardListController;
--- devblog/src/components/CardList/CardList.tsx
+++ devblog/src/components/CardList/CardList.tsx
@@ -1,6 +1,4 @@
-import matter from "gray-matter";
-import { get } from "https";
-import React, { useEffect, useRef} from "react";
+import React from "react";
 import BlogCard, { BlogData } from "./BlogCard/BlogCard";
 import CardListController from "./CardList.controller";
 import styles from './CardList.module.scss';
@@ -11,41 +9,13 @@ interface CardListProps {
     filter: string;
 }
 
-/*const data = {
-
-    data: {
-        slug: 'entry',
-        title: 'Making off del devblog',
-        intro: '¿Que tecnologías se han utilizado? ¿Qué pasos se han seguido para desarrollar la página? ¿Qué problemas me he enfrentado?',
-        date: '09 Jul 2022',
-        img: 'src de la imagen',
-        link: 'enlace a la entrada'
-    }, 
-    content: 'Contenido'
-}*/
-
 const CardList = ({ filter }: CardListProps) => {
 
-    const { getMarkdownContents, getMatter, entries, setEntries } = CardListController();
-
-    // React 18 Strict mode fuckery
-    const loadEntries = useRef(true);
-    useEffect(() => {
-        if(loadEntries.current){
-            getMarkdownContents()
-            loadEntries.current = false
-        }
-    }, [getMarkdownContents])
-
-    console.log(entries)
+    const { entrySet } = CardListController();   
 
     return (
         <div className={styles.container}>
             <h2 className={styles.header}>Entradas del blog</h2>
-
-            {//entries
-            }
-
             {
                 // NOTES: Here it should map an array of blog cards based on filtered titles
                 // If there is not filter on the searchbar, show pages of 10 cards each or something
@@ -53,8 +23,8 @@ const CardList = ({ filter }: CardListProps) => {
             }
 
             <div className={styles.cards}>
-            { entries.length ?
-                entries.map(entry => (
+            { entrySet ?
+                entrySet.map(entry => (
                     <BlogCard entryData={entry as BlogData} />
                 ))
             :
