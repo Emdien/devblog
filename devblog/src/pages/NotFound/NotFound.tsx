3e937c80175bd84e49e9c377e7c53c9b0a8cc0e8
Gonzalo Nicolás <gonnm8@gmail.com>
2022-08-01 14:25:15 +0200

    fix: removed unused import

--- devblog/src/pages/NotFound/NotFound.tsx
+++ devblog/src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from './NotFound.module.scss'
+//import styles from './NotFound.module.scss'
 
 const NotFound = () => (
     <div>404: Page not Found</div>
